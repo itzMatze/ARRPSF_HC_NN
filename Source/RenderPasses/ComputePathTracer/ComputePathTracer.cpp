2c1e2ec808544bc80e53266181ea1a6d878b8a0c
Matthias Bruns <matthiasbruns35@gmail.com>
2024-02-28 09:41:15 +0100

    add ComputePathTracer pass that is accessing the VBuffer

---
+++ Source/RenderPasses/ComputePathTracer/ComputePathTracer.cpp
@@ -0,0 +1,204 @@
+#include "ComputePathTracer.h"
+#include "RenderGraph/RenderPassHelpers.h"
+#include "RenderGraph/RenderPassStandardFlags.h"
+
+
+namespace
+{
+const std::string kShaderFile("RenderPasses/ComputePathTracer/ComputePathTracer.slang");
+
+const char kInputViewDir[] = "viewW";
+
+const ChannelList kInputChannels = {
+    // clang-format off
+    { "vbuffer",        "gVBuffer",     "Visibility buffer in packed format" },
+    { kInputViewDir,    "gViewW",       "World-space view direction (xyz float format)", true /* optional */ },
+    // clang-format on
+};
+
+const ChannelList kOutputChannels = {
+    // clang-format off
+    { "color",          "gOutputColor", "Output color (sum of direct and indirect)", false, ResourceFormat::RGBA32Float },
+    // clang-format on
+};
+
+const char kMaxBounces[] = "maxBounces";
+const char kComputeDirect[] = "computeDirect";
+const char kUseImportanceSampling[] = "useImportanceSampling";
+const char kTest[] = "test";
+} // namespace
+
+extern "C" FALCOR_API_EXPORT void registerPlugin(Falcor::PluginRegistry& registry)
+{
+    registry.registerClass<RenderPass, ComputePathTracer>();
+}
+
+ComputePathTracer::ComputePathTracer(ref<Device> pDevice, const Properties& props) : RenderPass(pDevice)
+{
+    for (const auto& [key, value] : props)
+    {
+        if (key == kMaxBounces)
+            mMaxBounces = value;
+        else if (key == kComputeDirect)
+            mComputeDirect = value;
+        else if (key == kUseImportanceSampling)
+            mUseImportanceSampling = value;
+        else
+            logWarning("Unknown property '{}' in ComputePathTracer properties.", key);
+    }
+}
+
+Properties ComputePathTracer::getProperties() const
+{
+    Properties props;
+    props[kMaxBounces] = mMaxBounces;
+    props[kComputeDirect] = mComputeDirect;
+    props[kUseImportanceSampling] = mUseImportanceSampling;
+    props[kTest] = mTest;
+    return props;
+}
+
+RenderPassReflection ComputePathTracer::reflect(const CompileData& compileData)
+{
+    RenderPassReflection reflector;
+
+    // Define our input/output channels.
+    addRenderPassInputs(reflector, kInputChannels);
+    addRenderPassOutputs(reflector, kOutputChannels);
+
+    return reflector;
+}
+
+void ComputePathTracer::compile(RenderContext* pRenderContext, const CompileData& compileData) {}
+
+void ComputePathTracer::execute(RenderContext* pRenderContext, const RenderData& renderData)
+{
+    const auto& pOutput = renderData.getTexture("color");
+    uint2 frameDim = {pOutput->getWidth(), pOutput->getHeight()};
+
+    // Update refresh flag if options that affect the output have changed.
+    auto& dict = renderData.getDictionary();
+    if (mOptionsChanged)
+    {
+        auto flags = dict.getValue(kRenderPassRefreshFlags, RenderPassRefreshFlags::None);
+        dict[Falcor::kRenderPassRefreshFlags] = flags | Falcor::RenderPassRefreshFlags::RenderOptionsChanged;
+        mOptionsChanged = false;
+    }
+
+    // If we have no scene, just clear the outputs and return.
+    if (!mpScene)
+    {
+        for (auto it : kOutputChannels)
+        {
+            Texture* pDst = renderData.getTexture(it.name).get();
+            if (pDst)
+                pRenderContext->clearTexture(pDst);
+        }
+        return;
+    }
+
+    // if (is_set(mpScene->getUpdates(), Scene::UpdateFlags::RecompileNeeded) ||
+    //     is_set(mpScene->getUpdates(), Scene::UpdateFlags::GeometryChanged))
+    // {
+    //     FALCOR_THROW("This render pass does not support scene changes that require shader recompilation.");
+    // }
+
+    // // Request the light collection if emissive lights are enabled.
+    // if (mpScene->getRenderSettings().useEmissiveLights)
+    // {
+    //     mpScene->getLightCollection(pRenderContext);
+    // }
+
+    // // Configure depth-of-field.
+    // const bool useDOF = mpScene->getCamera()->getApertureRadius() > 0.f;
+    // if (useDOF && renderData[kInputViewDir] == nullptr)
+    // {
+    //     logWarning("Depth-of-field requires the '{}' input. Expect incorrect shading.", kInputViewDir);
+    // }
+
+    DefineList defineList = getValidResourceDefines(kInputChannels, renderData);
+    defineList.add(getValidResourceDefines(kOutputChannels, renderData));
+    defineList.add(mpScene->getSceneDefines());
+    defineList["MAX_BOUNCES"] = std::to_string(mMaxBounces);
+    defineList["COMPUTE_DIRECT"] = mComputeDirect ? "1" : "0";
+    defineList["USE_IMPORTANCE_SAMPLING"] = mUseImportanceSampling ? "1" : "0";
+    defineList["TEST"] = mTest ? "1" : "0";
+    defineList["USE_ANALYTIC_LIGHTS"] = "1"; // mpScene->useAnalyticLights() ? "1" : "0");
+    defineList["USE_EMISSIVE_LIGHTS"] = "1"; // mpScene->useEmissiveLights() ? "1" : "0");
+    defineList["USE_ENV_LIGHT"] = "1"; // mpScene->useEnvLight() ? "1" : "0");
+    defineList["USE_ENV_BACKGROUND"] = "1"; // mpScene->useEnvBackground() ? "1" : "0");
+
+    if (!mpPass)
+    {
+        ProgramDesc desc;
+        desc.addShaderModules(mpScene->getShaderModules());
+        desc.addShaderLibrary(kShaderFile).csEntry("main");
+        desc.addTypeConformances(mpScene->getTypeConformances());
+
+        mpPass = ComputePass::create(mpDevice, desc, defineList, true);
+        mpVars = ProgramVars::create(mpDevice, mpPass->getProgram()->getReflector());
+    }
+
+    if (mpPass->getProgram()->addDefines(defineList))
+    {
+    // Set constants.
+        mpVars = ProgramVars::create(mpDevice, mpPass->getProgram()->getReflector());
+    }
+
+    auto var = mpVars->getRootVar();
+    var["CB"]["frameDim"] = frameDim;
+
+    // Bind I/O buffers. These needs to be done per-frame as the buffers may change anytime.
+    auto bind = [&](const ChannelDesc& desc)
+    {
+        if (!desc.texname.empty())
+        {
+            var[desc.texname] = renderData.getTexture(desc.name);
+        }
+    };
+    for (auto channel : kInputChannels)
+        bind(channel);
+    for (auto channel : kOutputChannels)
+        bind(channel);
+
+    mpPass->setVars(mpVars);
+    // Get dimensions of ray dispatch.
+    const uint2 targetDim = renderData.getDefaultTextureDims();
+    FALCOR_ASSERT(targetDim.x > 0 && targetDim.y > 0);
+
+    ref<ComputeState> cs = ComputeState::create(mpDevice);
+    // Spawn the rays.
+    mpPass->execute(pRenderContext, frameDim.x, frameDim.y);
+
+    mFrameCount++;
+}
+
+void ComputePathTracer::renderUI(Gui::Widgets& widget)
+{
+    bool dirty = false;
+
+    dirty |= widget.var("Max bounces", mMaxBounces, 0u, 1u << 16);
+    widget.tooltip("Maximum path length for indirect illumination.\n0 = direct only\n1 = one indirect bounce etc.", true);
+
+    dirty |= widget.checkbox("Evaluate direct illumination", mComputeDirect);
+    widget.tooltip("Compute direct illumination.\nIf disabled only indirect is computed (when max bounces > 0).", true);
+
+    dirty |= widget.checkbox("Use importance sampling", mUseImportanceSampling);
+    widget.tooltip("Use importance sampling for materials", true);
+
+    dirty |= widget.checkbox("Just some test checkbox", mTest);
+    widget.tooltip("Does something", true);
+
+    // If rendering options that modify the output have changed, set flag to indicate that.
+    // In execute() we will pass the flag to other passes for reset of temporal data etc.
+    if (dirty)
+    {
+        mOptionsChanged = true;
+    }
+}
+
+void ComputePathTracer::setScene(RenderContext* pRenderContext, const ref<Scene>& pScene)
+{
+    mpScene = pScene;
+}
+
