b2c4071289a531c50a9973e380b6a61f550acfd3
Matthias Bruns <matthiasbruns35@gmail.com>
2024-03-10 09:51:36 +0100

    structure ComputePT script such that it reflects the actual order of passes

--- scripts/ComputePathTracer.py
+++ scripts/ComputePathTracer.py
@@ -2,18 +2,19 @@ from falcor import *
 
 def render_graph_ComputePathTracer():
     g = RenderGraph("ComputePathTracer")
+    VBufferRT = createPass("VBufferRT", {'samplePattern': 'Stratified', 'sampleCount': 16})
+    g.addPass(VBufferRT, "VBufferRT")
+    ComputePathTracer = createPass("ComputePathTracer", {'maxBounces': 3})
+    g.addPass(ComputePathTracer, "ComputePathTracer")
     AccumulatePass = createPass("AccumulatePass", {'enabled': True, 'precisionMode': 'Single'})
     g.addPass(AccumulatePass, "AccumulatePass")
     ToneMapper = createPass("ToneMapper", {'autoExposure': False, 'exposureCompensation': 0.0})
     g.addPass(ToneMapper, "ToneMapper")
-    ComputePathTracer = createPass("ComputePathTracer", {'maxBounces': 3})
-    g.addPass(ComputePathTracer, "ComputePathTracer")
-    VBufferRT = createPass("VBufferRT", {'samplePattern': 'Stratified', 'sampleCount': 16})
-    g.addPass(VBufferRT, "VBufferRT")
-    g.addEdge("AccumulatePass.output", "ToneMapper.src")
+
     g.addEdge("VBufferRT.vbuffer", "ComputePathTracer.vbuffer")
     g.addEdge("VBufferRT.viewW", "ComputePathTracer.viewW")
     g.addEdge("ComputePathTracer.color", "AccumulatePass.input")
+    g.addEdge("AccumulatePass.output", "ToneMapper.src")
     g.markOutput("ToneMapper.dst")
     return g
 
