7a00551ce511273165b9233646b5f2c44a2ed878
Mikhail Dereviannykh <devmishok@gmail.com>
2024-07-21 21:11:42 +0200

    path fix only for Mike

---
+++ scripts/ComputePathTracerMike.py
@@ -0,0 +1,24 @@
+from falcor import *
+
+def render_graph_ComputePathTracer(options = {}):
+    passes = {}
+    passes['ImageLoader'] = createPass("ImageLoader", {'filename': 'D:/GradientDiffusion/gradientdiffusion/Experiments/FinalEX2/BistroDirection/nirc_1_converged.png'})
+    passes['VBufferRT'] = createPass("VBufferRT", {'samplePattern': 'Stratified', 'sampleCount': 16})
+    passes['ComputePathTracer'] = createPass("ComputePathTracer", options)
+    passes['AccumulatePass'] = createPass("AccumulatePass", {'enabled': False, 'precisionMode': 'Single'})
+    passes['ToneMapper'] = createPass("ToneMapper", {'autoExposure': False, 'exposureCompensation': 0.0})
+    g = RenderGraph("ComputePathTracer")
+    for key, value in passes.items():
+        g.addPass(value, key)
+
+    g.addEdge("ImageLoader.dst", "ComputePathTracer.refImage")
+    g.addEdge("VBufferRT.vbuffer", "ComputePathTracer.vbuffer")
+    g.addEdge("VBufferRT.viewW", "ComputePathTracer.viewW")
+    g.addEdge("ComputePathTracer.color", "AccumulatePass.input")
+    g.addEdge("AccumulatePass.output", "ToneMapper.src")
+    g.markOutput("ToneMapper.dst")
+    return g, passes
+
+ComputePathTracer, _ = render_graph_ComputePathTracer()
+try: m.addGraph(ComputePathTracer)
+except NameError: None
